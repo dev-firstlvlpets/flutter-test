063370584104154900c4cad3a3fb9d6937ca7cb8
Alessandro <retes999@gmail.com>
2024-04-15 16:46:37 +0200

    changes

--- lib/src/shared/env.dart
+++ lib/src/shared/env.dart
@@ -6,4 +6,10 @@ part 'env.g.dart';
 final class Env {
   @EnviedField(varName: 'SENTRY_DSN', obfuscate: true)
   static final String sentryDsn = _Env.sentryDsn;
+
+  @EnviedField(varName: 'SUPABASE_URL', obfuscate: true)
+  static final String supabaseUrl = _Env.supabaseUrl;
+
+  @EnviedField(varName: 'SUPABASE_ANON_KEY', obfuscate: true)
+  static final String supabaseAnonKey = _Env.supabaseAnonKey;
 }
